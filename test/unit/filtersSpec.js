1185ab076da93161acfbf5a6cdcf8e6af687b792
stefan-00 <stefan.akerberg@gmail.com>
2013-02-17 16:01:12 +0100

    some cleanup

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for filters go here */
 
 describe('filter', function() {
-  beforeEach(module('racingApp.filters'));
+  beforeEach(module('pytimer.filters'));
 
   /*
   describe('interpolate', function() {
